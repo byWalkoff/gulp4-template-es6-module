4c245bee1d5591c230fcb30d8d1dd368d9132ec2
Igor Walkoff <bywalkoff@gmail.com>
2020-05-15 20:40:23 +0300

    dependencies updated; example of google font implementation added

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,7 +5,7 @@ module.exports = function () {
 			.pipe($.plumber())
 			.pipe($.sass())
 			.pipe(
-				$.postcss([$.autoprefixer({browsers: ['last 2 versions', 'ie >= 10', 'Android >= 4.4', 'Safari >= 8', 'iOS >= 7']}), $.mqpacker({sort: true})]))
+				$.postcss([$.autoprefixer(), $.mqpacker({sort: true})]))
 			.pipe($.gulpIf($.isDevelopment, $.sourcemaps.write()))
 			.pipe($.gulp.dest('frontend/dist/css'))
 			.pipe($.browserSync.reload({stream: true}));
